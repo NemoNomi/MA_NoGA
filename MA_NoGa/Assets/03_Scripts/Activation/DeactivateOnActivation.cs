e469b31c1be84692c7a3f679ebd525d62d0b7f44
NemoNomi <162903221+NemoNomi@users.noreply.github.com>
2025-06-17 21:27:19 +0200

    adds Inactive Doors to Academy

---
+++ MA_NoGa/Assets/03_Scripts/Activation/DeactivateOnActivation.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+/// <summary>
+/// When the GameObject that owns this component becomes active,
+/// it disables another GameObject.  
+/// </summary>
+
+public class DeactivateOnActivation : MonoBehaviour
+{
+    [Tooltip("This object will be turned OFF when the trigger object turns ON.")]
+    [SerializeField] private GameObject objectToDeactivate;
+
+
+    private void OnEnable()
+    {
+        if (objectToDeactivate != null)
+            objectToDeactivate.SetActive(false);
+    }
+
+}
