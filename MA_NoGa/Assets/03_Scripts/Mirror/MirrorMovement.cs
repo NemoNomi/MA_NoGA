73c79b52324fea1a34ee97e0cce0310c09aea647
NemoNomi <162903221+NemoNomi@users.noreply.github.com>
2025-06-18 17:45:51 +0200

    adds Mirror to last Scene

---
+++ MA_NoGa/Assets/03_Scripts/Mirror/MirrorMovement.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+/// <summary>
+/// Inverts axis for Mirror.
+/// </summary>
+
+public class MirrorMovement : MonoBehaviour
+{
+    public Transform playerTarget;
+    public Transform mirror;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        Vector3 localPlayer = mirror.InverseTransformPoint(playerTarget.position);
+        transform.position = mirror.TransformPoint(new Vector3(localPlayer.x, localPlayer.y, -localPlayer.z));
+
+        Vector3 lookAtMirror = mirror.TransformPoint(new Vector3(-localPlayer.x, localPlayer.y, localPlayer.z));
+        transform.LookAt(lookAtMirror);
+    }
+}
